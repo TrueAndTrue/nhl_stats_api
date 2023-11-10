bb11cefe812bc6f5eac3de5de7fe8541d2052345
Austin Westbury <austinwestbury@live.com>
2023-11-09 22:23:52 -0500

    player db model

--- players/models.py
+++ players/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Player(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  nhl_api_id = models.IntegerField()
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  is_active = models.BooleanField()
+  jersey_number = models.IntegerField()
+  primary_position = models.CharField(max_length=255)
+  headshot = models.CharField(max_length=255)
+  hero_image = models.CharField(max_length=255)
+  birth_date = models.DateField()
+  height_in_cm = models.IntegerField()
+  weight_in_lbs = models.IntegerField()
